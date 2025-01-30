73f823694b9904762d067e20366685bc398b5d9c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2025-01-30 14:37:16 -0600

    First commit

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require "debug"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb :home
+end
+
+get("/pokedex") do
+  erb :pokedex
+end
+
+get("/pokedex/:name") do
+  @name = params[:name]
+
+  API = "https://pokeapi.co/api/v2/pokemon/#{@name}"
+
+  req = HTTP.get(API)
+
+  @res = JSON.parse(req)
+
+  @name = @res["name"]
+
+  @moves = @res.dig("moves")
+
+  @photo = @res.dig("sprites", "front_default")
+
+  @weight = @res.dig("weight")
+
+  erb :pokedex
 end
---
+++ functional-specs.md
@@ -0,0 +1,13 @@
+# Functional Specs
+
+## User Stories
+
+- I want to find a pokemon by its name including its type and abilities
+- I want to see a list of all pokemon
+- I want to see a random pokemon
+- I want to see it in a pokedex
+
+## API Usage
+
+Get a Pokemon by its name - `https://pokeapi.co/api/v2/pokemon/#{name}`
+Get a list of all Pokemon - `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`
